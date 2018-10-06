b5d4fd7a30b2813b27057197d3bf4e0266a49d95
Samuel Allemang <samuel.allemang@gmail.com>
2018-10-06 07:33:57 -0400

    10 mins into vid 17

---
+++ sick-fits/frontend/components/CreateItem.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import Form from './styles/Form';
+import formatMoney from '../lib/formatMoney';
+
+class CreateItem extends Component {
+	state = {
+		title: 'Cool Shoes',
+		description: '',
+		image: '',
+		largeImage: '',
+		price: 0,
+	}
+	handleChange = (e) => {
+		const { name, type, value } = e.target;
+		const val = type === 'number' ? parseFloat( value ) : value;
+		this.setState({
+			[name]: val, 
+		});
+	}
+    render() {
+        return (
+            <Form>
+            	<fieldset>
+            		<label htmlFor="title">
+            			Title
+            			<input 
+	            			type="text" 
+	            			id="title" 
+	            			name="title" 
+	            			placeholder="Title" 
+	            			required 
+	            			value={ this.state.title }
+	            			onChange={ this.handleChange }
+	            		/>
+	            		<input 
+	            			type="number" 
+	            			id="price" 
+	            			name="price" 
+	            			placeholder="Price" 
+	            			required 
+	            			value={ this.state.price }
+	            			onChange={ this.handleChange }
+	            		/>
+            		</label>
+            	</fieldset>
+            </Form>
+        );
+    }
+}
+
+export default CreateItem;
